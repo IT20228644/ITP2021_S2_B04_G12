58369882494fdab80195f06565d2b880d59b5047
sajiniPW <it20418274@my.sliit.lk>
2021-09-20 10:11:40 +0530

    GeneralBillDAO added

---
+++ src/main/java/lk/sliit/hotel/dao/reservationDAO/GeneralBillDAO.java
@@ -0,0 +1,7 @@
+package lk.sliit.hotel.dao.reservationDAO;
+
+import lk.sliit.hotelManagement.entity.reservation.GeneralBill;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GeneralBillDAO extends CrudRepository<GeneralBill,Integer> {
+}
