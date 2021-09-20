10229b6e59cc2570f5b1389719dacaf18a8ea80c
sajiniPW <it20418274@my.sliit.lk>
2021-09-20 21:47:32 +0530

    reservationController updated

--- src/main/java/lk/sliit/hotel/controller/reservationController/ReservationController.java
+++ src/main/java/lk/sliit/hotel/controller/reservationController/ReservationController.java
@@ -1,21 +1,29 @@
 package lk.sliit.hotel.controller.reservationController;
 
 import lk.sliit.hotel.controller.SuperController;
+import lk.sliit.hotel.dto.reservation.ReservationDTO;
 import lk.sliit.hotel.service.custom.IndexLoginBO;
+import lk.sliit.hotel.service.custom.ReservationBO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ReservationController {
 
     @Autowired
     IndexLoginBO indexLoginBO;
+    @Autowired
+    ReservationBO reservationBO;
 
     @GetMapping("/reservation")
     public String loginPage(Model model){
         model.addAttribute("loggerName", indexLoginBO.getEmployeeByIdNo(SuperController.idNo));
+        List<ReservationDTO> k= reservationBO.findAllRooms();
+        model.addAttribute("loadAllTable", k);
         return "reservation";
     }
 
